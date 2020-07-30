f5bfa23fa76e746f7d7c4c46164cfd50b06c351c
Mike England <mike.england@webdevstudios.com>
2020-07-30 08:19:31 -0400

    Remove additional effects

--- src/utilities/jimp/ImageManipulation.js
+++ src/utilities/jimp/ImageManipulation.js
@@ -10,8 +10,6 @@ const resizeImage = (imageFile, callback) => {
 	Jimp.read( URL.createObjectURL(imageFile) )
 	.then( image => (
 		image
-			.scaleToFit(800, 800)
-			.quality(80)
 			.getBuffer(Jimp.AUTO, (err, buffer) => {
 				// Convert buffer to new image file.
 				const optimizedImage = new File([buffer], imageFile.name, {
